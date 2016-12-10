cc1dd248866fb9e35fd5f5a34ad085f102c8124b
Nguyen Van Hai <coc.fuhn@gmail.com>
2016-12-10 17:49:33 +0900

    user suggestions list

--- server_code/app/Http/Controllers/PhotoController.php
+++ server_code/app/Http/Controllers/PhotoController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Storage;
 
 class PhotoController extends Controller
 {
+    const MAPPING_AGE = 3;
+    const MALE = "MALE";
+    const FEMALE= "FEMALE";
     //
     public function uploadForm()
     {
@@ -23,6 +26,9 @@ class PhotoController extends Controller
 
 //            return $realPath;
             $faceInfo = $this->getFaceAPI($realPath);
+            $faceId = "7f03ba8d-22d7-4976-847f-c9da0bd1a1bf";
+            $faceIdList = "f114c8e1-394d-45ec-88ec-13c96dbbb053, aa24f7da-a1c1-4834-bb94-f9c907c7ee54, 49d77c8d-8b1c-4cf2-9600-6ccafa0b1c39,2aa863b0-faaa-44c8-b319-9848a87d71ac";
+            $faceFamilia = $this->getFaceFimiliar($faceId, $faceIdList);
 
             return $faceInfo;
         }
@@ -67,6 +73,99 @@ class PhotoController extends Controller
         } catch (\HttpException $ex) {
             return $ex;
         }
+    }
+
+    public function getFaceFimiliar($faceId, $faceList){
+        $request = new Http_Request2('https://api.projectoxford.ai/face/v1.0/findsimilars');
+        $url = $request->getUrl();
+
+        $headers = array(
+            // Request headers
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => '2968a3fde555436aad8348f13a82a108',
+        );
+
+        $request->setHeader($headers);
+
+        $parameters = array(
+            // Request parameters
+        );
+
+        $url->setQueryVariables($parameters);
+
+        $request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body
+        $request->setBody("{
+
+        \"faceId\":\"$faceId\",
+        \"faceListId\":\"$faceList\",
+        \"maxNumOfCandidatesReturned\":10,
+        \"mode\": \"matchFace\"
+        }");
+
+        try
+        {
+            $response = $request->send();
+            echo $response->getBody();
+        }
+        catch (HttpException $ex)
+        {
+            echo $ex;
+        }
+    }
+
+
+
+    function getListFacesData() {
+        $facesData = array();
+        $facesData = User::all();
+        return $facesData;
+    }
+
+    function isMappingUser($own, $other) {
+        $isMap = false;
+        $diff_age = $own["AGE"] - $other["AGE"];
+        if (abs($diff_age) <= MAPPING_AGE) {
+            $isMap = true;
+        }
+        if($isMap){
+            if($own["GENDER"] == MALE || $own["GENDER" == FEMALE]){
+                if($own["GENDER"] == $other["GENDER"]){
+                    $isMap = false;
+                }
+            }
+
+        }
+        else
+        {
+            $isMap = true;
+        }
+        return false;
+    }
+
+    function isSameUser($own, $other) {
+        $same = true;
+        foreach ($own as $key => $value) {
+            if($other[$key] != $value){
+                $same = false;
+            }
+        }
+        return $same;
+    }
+
+    function getListUsersSuggestion($own) {
+        $usersSuggest = array();
+        $usersList = getListFacesData();
+        foreach ($usersList as $key => $other) {
+            if(isSameUser($own, $other)){
+                continue;
+            }
+            if(isMappingUser($own, $other)){
+                $usersSuggest[$key] = $other;
+            }
+        }
+        return $usersSuggest;
     }
 
 }
