3b62899bf85d864dfa08ae0178fee81352603c44
lvt <zero.lethanh@gmail.com>
2016-12-10 12:45:48 +0900

    Initial Commit

---
+++ XmasDating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XmasDating
+//
+//  Created by THANH LEVAN on 2016/12/10.
+//  Copyright © 2016年 THANH LEVAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
